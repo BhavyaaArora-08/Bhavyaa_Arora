efd988cacd07ac5cc625d6a30b3d720b67eaaeb9
Bhavyaa Arora <44175278+Bhavyaa08@users.noreply.github.com>
2020-08-04 17:28:30 +0530

    Initial Commit

---
+++ card.js
@@ -0,0 +1,68 @@
+// Refs
+const wrapCta = document.querySelector("#wrap-cta"),
+  btnCta = document.querySelector("#cta"),
+  content = document.querySelector("#content"),
+  btnClose = document.querySelector("#close");
+
+// Anime.js Commons Values for SVG Morph
+const common = {
+  targets: ".polymorph",
+  easing: "easeOutQuad",
+  duration: 600,
+  loop: false,
+};
+
+// Show content
+btnCta.addEventListener("click", () => {
+  // Elements apparence
+  wrapCta.classList.remove("active");
+  content.classList.add("active");
+
+  // Morph SVG
+  anime({
+    ...common,
+    points: [{ value: "215,110 0,110 186,86 215,0" }],
+  });
+});
+
+// Hide content
+btnClose.addEventListener("click", () => {
+  // Elements apparence
+  content.classList.remove("active");
+  wrapCta.classList.add("active");
+
+  // Morph SVG
+  anime({
+    ...common,
+    points: [{ value: "215,110 0,110 0,0 215,0" }],
+  });
+});
+
+const cards = document.querySelectorAll(".card");
+console.log(cards);
+
+var playing = [];
+
+for (var i = 0; i < cards.length; i++) {
+  playing[i] = false;
+}
+
+for (var i = 0; i < cards.length; i++) {
+  const card = cards[i];
+  console.log(card);
+  card.addEventListener("click", function () {
+    if (playing[i]) return;
+
+    playing[i] = true;
+    anime({
+      targets: card,
+      scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+      rotateY: { value: "+=180", delay: 200 },
+      easing: "easeInOutSine",
+      duration: 400,
+      complete: function (anim) {
+        playing[i] = false;
+      },
+    });
+  });
+}
---
+++ main.js
@@ -0,0 +1,29 @@
+// we have to create a function to get rid of the preloader
+
+// when the whole website loads
+window.addEventListener("load", async () => {
+  setTimeout(() => {
+    $(".preloader .rocket").addClass("rocket-exit");
+    $(".preloader h1").addClass("rocket-exit");
+  }, 1200);
+
+  $(".preloader").fadeOut(1500, () => {
+    $(".content").removeClass("hide");
+    $("#my-section").removeClass("hide");
+    $(".container1").removeClass("hide");
+  });
+  let bg = document.querySelector("#bg");
+  let moon = document.querySelector("#moon");
+  // let mountain = document.querySelector("#mountain");
+  let road = document.querySelector("#road");
+  let text = document.querySelector("#text");
+
+  window.addEventListener("scroll", () => {
+    var value = window.scrollY;
+    bg.style.top = value * 0.5 + "px";
+    moon.style.left = -value * 0.5 + "px";
+    mountain.style.top = -value * 0.2 + "px";
+    road.style.top = -value * 0 + "px";
+    text.style.top = value * 0.65 + "px";
+  });
+});
